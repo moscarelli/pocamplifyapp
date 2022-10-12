c01fe8eac0c3d591876cc8922c090030af1537ff
moscarelli <moscarelli@gmail.com>
2022-10-12 10:14:59 -0300

    new button

---
+++ src/pirng.js
@@ -0,0 +1,19 @@
+const  printPDF = (url) => {
+    {
+        var iframe = document.createElement('iframe');
+        iframe.id = 'pdfIframe'
+        iframe.className='pdfIframe'
+        document.body.appendChild(iframe);
+        iframe.style.display = 'none';
+        iframe.onload = function () {
+            setTimeout(function () {
+                iframe.focus();
+                iframe.contentWindow.print();
+                URL.revokeObjectURL(url)
+                document.body.removeChild(iframe)
+            }, 1);
+        };
+        iframe.src = url;
+    }
+    };
+    export default printPDF;
