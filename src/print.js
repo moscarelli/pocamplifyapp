63eab5a4bf5886f00df2fb165b7185671b3eb296
moscarelli <moscarelli@gmail.com>
2022-10-12 18:18:22 -0300

    Update print.js

--- src/print.js
+++ src/print.js
@@ -1,19 +1,20 @@
 const  printPDF = (url) => {
     {
-        var iframe = document.createElement('iframe');
-        iframe.id = 'pdfIframe'
-        iframe.className='pdfIframe'
-        document.body.appendChild(iframe);
-        iframe.style.display = 'none';
-        iframe.onload = function () {
-            setTimeout(function () {
-                iframe.focus();
-                iframe.contentWindow.print();
-                URL.revokeObjectURL(url)
-                document.body.removeChild(iframe)
-            }, 1);
-        };
-        iframe.src = url;
+        // var iframe = document.createElement('iframe');
+        // iframe.id = 'pdfIframe'
+        // iframe.className='pdfIframe'
+        // document.body.appendChild(iframe);
+        // iframe.style.display = 'none';
+        // iframe.onload = function () {
+        //     setTimeout(function () {
+        //         iframe.focus();
+        //         iframe.contentWindow.print();
+        //         URL.revokeObjectURL(url)
+        //         document.body.removeChild(iframe)
+        //     }, 1);
+        // };
+        // iframe.src = url;
+        window.print();
     }
     };
     export default printPDF;
