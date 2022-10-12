580f4a9b45e405808044c26ce032b617f2316a26
moscarelli <moscarelli@gmail.com>
2022-10-12 17:56:31 -0300

    fixed full pdf

--- src/App.js
+++ src/App.js
@@ -5,28 +5,33 @@ import printPDF  from'./print'
 import SomeContent from './somecontent';
 
 function App() {
-  return (
-    <div id="divToDownload">  
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+  return (    
+      <div id="divToDownload" className="App">
+        <header className="App-header"  >
+          <img src={logo} className="App-logo" alt="logo" />          
           <p>
             Edit <code>src/App.js</code> and save to reload.
-          </p>  
+          </p>
+          <img src="ceu2.jpg" />
+          <p>First Table - Should Show on PDF</p> 
+          <SomeContent></SomeContent> 
+          <p></p> 
           <button onClick={() => printPDF("/")}> Print PDF API/Browser Call</button>              
           <GenericPdfDownloader 
             downloadFileName="CustomPdf" 
             rootElementId="divToDownload" 
-            buttonText = "JSPD Full Page PDF"
+            buttonText = "JSPDF Full Page PDF"
           />
           <img src="ceu.jpg" />
           <div id="testId"> 
               This is A Downloadable Component  Should be Visible on pdf!!!
           </div>
-        </header>
-        <SomeContent></SomeContent>
-      </div>
-    </div>
+          <p>Second Table - Should Show on PDF</p> 
+          <SomeContent></SomeContent> 
+          <p></p> 
+          <img src="download.jpg" />
+        </header>        
+      </div>    
   );
 }
 
--- src/GenericPdfDownloader.js
+++ src/GenericPdfDownloader.js
@@ -5,19 +5,36 @@ import { jsPDF } from "jspdf";
 
 const GenericPdfDownloader = ({rootElementId , downloadFileName,buttonText}) => {
 
+    // const downloadPdfDocument = () => {
+    //     const input = document.getElementById(rootElementId);
+    //     html2canvas(input)
+    //         .then((canvas) => {
+    //             //canvas.setAttribute('width', 1080);
+    //             //canvas.setAttribute('height', 1920);
+    //             const imgData = canvas.toDataURL('image/png');
+    //             const pdf = new jsPDF();
+    //             pdf.addImage(imgData, 'JPEG', 0,0);                
+    //             pdf.save(`${downloadFileName}.pdf`);
+    //         })
+    // }
+
     const downloadPdfDocument = () => {
         const input = document.getElementById(rootElementId);
-        html2canvas(input)
-            .then((canvas) => {
-                //canvas.setAttribute('width', 1080);
-                //canvas.setAttribute('height', 1920);
-                const imgData = canvas.toDataURL('image/png');
-                const pdf = new jsPDF();
-                pdf.addImage(imgData, 'JPEG', 0,0);                
-                pdf.save(`${downloadFileName}.pdf`);
-            })
-    }
+        html2canvas(input).then((canvas)=>{           
 
+                //Returns the image data URL, parameter: image format and clarity (0-1)
+                var pageData = canvas.toDataURL('image/png', 1.0);
+
+                //Default vertical direction, size ponits, format a4[595.28,841.89]
+                var pdf = new jsPDF('p', 'pt', 'a4');
+
+                //Two parameters after addImage control the size of the added image, where the page height is compressed according to the width-height ratio column of a4 paper.
+                pdf.addImage(pageData, 'JPEG', 0, 0, 595.28, 592.28/canvas.width * canvas.height );
+
+                pdf.save('stone.pdf');            
+        })
+    }
+     
     return <button onClick={downloadPdfDocument}>{buttonText}</button>
 
 }
