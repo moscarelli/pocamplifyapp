2aa7af69f25427660f25f29be8cbe2ab5ba4b556
moscarelli <moscarelli@gmail.com>
2022-10-12 12:08:45 -0300

    add pdf library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import printPDF  from'./print'
+import GenericPdfDownloader from "./GenericPdfDownloader";
 
 function App() {
   return (
+    <div id="divToDownload">  
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={() => printPDF("/")}> Print PDF</button>
+        </p>       
+        <GenericPdfDownloader 
+          downloadFileName="CustomPdf" 
+          rootElementId="testId" 
+        >Test Div</GenericPdfDownloader>
+        <GenericPdfDownloader 
+          downloadFileName="CustomPdf" 
+          rootElementId="divToDownload" 
+        >Full</GenericPdfDownloader>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React          
         </a>
+        <div id="testId"> 
+            This is A Downloadable Component 
+        </div>
       </header>
+    </div>
     </div>
   );
 }
